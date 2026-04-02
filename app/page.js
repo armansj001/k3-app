fba99986ca301936ded31f15ec51a49e754d93a5
armansj001 <155941142+armansj001@users.noreply.github.com>
2026-04-02 19:02:59 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import { db } from "../../lib/firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+
 export default function Home() {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = async () => {
+    if (!title) {
+      alert("Judul tidak boleh kosong");
+      return;
+    }
+
+    try {
+      await addDoc(collection(db, "incidents"), {
+        title: title,
+        createdAt: serverTimestamp(),
+      });
+
+      alert("Berhasil disimpan ✅");
+      setTitle("");
+    } catch (error) {
+      console.error(error);
+      alert("Gagal menyimpan ❌");
+    }
+  };
+
   return (
     <main style={{ padding: 20 }}>
       <h1>Aplikasi K3</h1>
 
-      <input placeholder="Judul kejadian" />
+      <input
+        placeholder="Judul kejadian"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
       <br /><br />
 
-      <button>Kirim Laporan</button>
+      <button onClick={handleSubmit}>
+        Kirim Laporan
+      </button>
 
       <hr style={{ margin: "20px 0" }} />
 
@@ -15,4 +50,4 @@ export default function Home() {
       </a>
     </main>
   );
-}
+    }
