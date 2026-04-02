353fa3789cb94a56df8e651eea866b4ed54bad50
armansj001 <155941142+armansj001@users.noreply.github.com>
2026-04-02 23:07:07 +0700

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { auth } from "../../lib/firebase";
+import { auth } from "../../lib/firebaseAuth";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
 
