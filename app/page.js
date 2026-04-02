5f5a81281ab1203671a07f05253a986a2c634099
armansj001 <155941142+armansj001@users.noreply.github.com>
2026-04-02 11:28:02 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,39 @@
+"use client";
+
+import { useState } from "react";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import app from "../lib/firebase";
+
 export default function Home() {
-  return <h1>Aplikasi K3 Jalan 🚀</h1>;
+  const [title, setTitle] = useState("");
+
+  const submit = async () => {
+    const db = getFirestore(app);
+
+    await addDoc(collection(db, "incidents"), {
+      title: title,
+      createdAt: new Date()
+    });
+
+    alert("Laporan berhasil dikirim!");
+    setTitle("");
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Aplikasi K3</h1>
+
+      <input
+        placeholder="Judul kejadian"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={submit}>
+        Kirim Laporan
+      </button>
+    </div>
+  );
 }
