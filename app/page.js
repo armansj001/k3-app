d8c12035936864713dff358fdb9f399828a5fd30
armansj001 <155941142+armansj001@users.noreply.github.com>
2026-04-02 18:09:06 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,27 +3,16 @@ export default function Home() {
     <main style={{ padding: 20 }}>
       <h1>Aplikasi K3</h1>
 
-      <p>
-        Sistem pelaporan dan monitoring keselamatan kerja (K3)
-      </p>
+      <input placeholder="Judul kejadian" />
+      <br /><br />
+
+      <button>Kirim Laporan</button>
 
       <hr style={{ margin: "20px 0" }} />
 
-      <h2>Menu</h2>
-
-      <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-        <a href="/dashboard">
-          <button style={{ padding: 10, width: "100%" }}>
-            📊 Dashboard
-          </button>
-        </a>
-
-        <a href="/report">
-          <button style={{ padding: 10, width: "100%" }}>
-            ➕ Buat Laporan
-          </button>
-        </a>
-      </div>
+      <a href="/dashboard">
+        <button>Lihat Dashboard</button>
+      </a>
     </main>
   );
 }
